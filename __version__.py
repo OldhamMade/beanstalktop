8ddf95a45486edc5e8ae3aa9477bedc295147308
Phillip Oldham <phillip.oldham@gmail.com>
2013-09-11 15:19:06 +0100

    fixing setup to work with python 2.6

--- __version__.py
+++ __version__.py
@@ -1,3 +1,3 @@
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __author__ = 'Phillip B Oldham'
 __licence__ = 'MIT'
--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup, find_packages
 import os
 
-execfile('__version__.py')
-
 setup(name="beanstalktop",
       description="A simple, top-like monitoring tool for beanstalkd",
       long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
       url='https://github.com/wewriteapps/beanstalktop',
-      author=__author__,
-      version=__version__,
+      author='Phillip B Oldham',
+      version='0.0.4',
       entry_points={
           'console_scripts': [
               'beanstalktop = beanstalktop:main',
