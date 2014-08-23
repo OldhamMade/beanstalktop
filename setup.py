4ddecb9fc1a2cf7cb513f1c3298a725fbec4490f
Simeon Visser <svisser@users.noreply.github.com>
2014-08-23 16:24:40 +0100

    Ensure file handle is closed using with statement

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup, find_packages
 import os
 
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as file_readme:
+      long_description = file_readme.read()
+
 setup(name="beanstalktop",
       description="A simple, top-like monitoring tool for beanstalkd",
-      long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+      long_description=long_description,
       url='https://github.com/wewriteapps/beanstalktop',
       author='Phillip B Oldham',
       version='0.0.4',
