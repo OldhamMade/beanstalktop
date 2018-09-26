3d032220c6edcdf3583381ecde8aa6bbddfb7bb6
Phillip Oldham <phillip.oldham@gmail.com>
2018-09-26 10:10:38 +0100

    Add Licence and update to semver versioning.

--- README.rst
+++ README.rst
@@ -6,6 +6,9 @@ DESCRIPTION
 
 beanstalktop is a simple, top-like UI for monitoring beanstalkd_ servers.
 
+**NOTE:** This project is no longer under active development, however PRs
+are welcome and will be reviewed and merged as quickly as possible.
+
 
 TO DO
 -----
--- setup.py
+++ setup.py
@@ -1,23 +1,29 @@
 from setuptools import setup
 import os
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as file_readme:
-      long_description = file_readme.read()
+VERSION_FILE = '__version__.py'
 
-setup(name="beanstalktop",
-      description="A simple, top-like monitoring tool for beanstalkd",
-      long_description=long_description,
-      url='https://github.com/wewriteapps/beanstalktop',
-      author='Phillip B Oldham',
-      version='0.0.4',
-      entry_points={
-          'console_scripts': [
-              'beanstalktop = beanstalktop:main',
-              ],
-          },
-      py_modules=['beanstalktop'],
-      install_requires=[
-          'beanstalkc',
-          'PyYAML',
-          ],
-      )
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as file_readme:
+    long_description = file_readme.read()
+
+exec(compile(open(VERSION_FILE).read(), VERSION_FILE, 'exec'))
+
+setup(
+    name="beanstalktop",
+    description="A simple, top-like monitoring tool for beanstalkd",
+    long_description=long_description,
+    url='https://github.com/OldhamMade/beanstalktop',
+    author=__author__,
+    version=__version__,
+    license=__licence__,
+    entry_points={
+        'console_scripts': [
+            'beanstalktop = beanstalktop:main',
+        ],
+    },
+    py_modules=['beanstalktop'],
+    install_requires=[
+        'beanstalkc',
+        'PyYAML',
+    ],
+)
